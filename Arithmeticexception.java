c7dcc676f41b3f79580b0e8e97ceaf7c1ad9a4d1
sb4754 <115706611+sb4754@users.noreply.github.com>
2022-11-04 23:37:09 +0530

    Add files via upload

---
+++ Arithmeticexception.java
@@ -0,0 +1,14 @@
+package com.exception;
+public class Arithmeticexception
+{ public static void main(String[] args) {
+    int num1 = 6,num2 = 0, results;
+    try{
+        results = num1/num2;
+        System.out.println("the result is "+results);
+    }
+    catch(ArithmeticException e)
+    {
+        System.out.println("Can't be divided by zero " +e);
+    }
+}
+}
