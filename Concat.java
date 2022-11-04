788b3f0841532325307c1d54386e865d4f24fc3d
sb4754 <115706611+sb4754@users.noreply.github.com>
2022-11-04 23:33:41 +0530

    Add files via upload

---
+++ Concat.java
@@ -0,0 +1,10 @@
+package com.string;
+
+public class Concat {
+            public static void main(String[] args) {
+            String s1="Hello";
+            String s2=" How are you?";
+            String s=s1.concat(s2);
+            System.out.println(s);
+        }
+}
